9b1a2d3fb8ee8ff1cc61a157400e9771bf6f432e
NataliaMigallon <natmigallon@gmail.com>
2021-01-28 11:00:15 +0100

    add second commit

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//Siento no haber podido hacer el ejercicio. Me he bloqueado y no sé ni lo que tengo que hacer. 
+
+const myNumberElement = document.querySelector('.js-input-mynumber');
+const buttonElement = document.querySelector('.js-button');
+
+
+function getRandomNumber(max) {
+    console.log(Math.ceil);
+    return Math.ceil(Math.random() * max);
+}
+
+function handleButton(event) {
+    const buttonElementValue = event.target.value;
+    console.log('Me han clickado');
+}
+
+buttonElement.addEventListener('click', handleButton)
+
+function handleMyNumber(event) {
+    const myNumberElementValue = event.target.value;
+    console.log(myNumberElement);
+}
+
+myNumberElement.addEventListener('keyup', handleMyNumber)
